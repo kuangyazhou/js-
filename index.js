96a5aa819bf7afb016737d17c79ffa5784e2909a
kuangyazhou <18707174713@139.com>
2017-11-04 18:16:50 +0800

    ''

--- index.js
+++ index.js
@@ -26,3 +26,33 @@ class List {
         this.contains = contains;
     }
 }
+
+class Person {
+    constructor(first, middle, last) {
+        this.first = first;
+        this.middle = middle;
+        this.last = last;
+    };
+    fullName() {
+        console.log(this.first + this.middle + this.last)
+    }
+}
+
+let paul = new Person(1, 2, 3);
+paul.fullName();
+
+//选择排序
+function selectionsort() {
+    var min, temp;
+    for (var outer = 0; outer <= this.dataStore.length - 2; ++outer) {
+        min = outer;
+        for (var inner = outer + 1; inner <= this.dataStore.length - 1; ++inner) {
+            if (this.dataStore[inner] < this.dataStore[min]) {
+                min = inner;
+            }
+        }
+        temp = this.dataStore[outer];
+        this.dataStore[outer] = this.dataStore[min];
+        this.dataStore[min] = temp;
+    }
+}
